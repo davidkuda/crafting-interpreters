4b437071fc7e50603c6af26052c5e6b5bbc279b1
David Kuda <davidkuda3@gmail.com>
2026-03-08 17:39:06 +0100

    test: add test for comments

--- golox/cmd/cli/scanner_test.go
+++ golox/cmd/cli/scanner_test.go
@@ -31,3 +31,40 @@ func TestScanSingleCharacterToken(t *testing.T) {
 		t.FailNow()
 	}
 }
+
+func TestScanComment(t *testing.T) {
+	var input string
+	var s Scanner
+	var failed bool
+
+	input = "(*) // this lexeme is a comment (without further tokens)\n\t( + )"
+	s = NewScanner([]byte(input))
+	s.ScanTokens()
+	if s.Tokens[0].Type != SLASH {}
+	if s.Tokens[0].Literal != input[3:] {}
+
+	expected := []Token{
+		{LEFT_PAREN, "(", "", 1},
+		{STAR, "*", "", 1},
+		{RIGHT_PAREN, ")", "", 1},
+		{LEFT_PAREN, "(", "", 2},
+		{PLUS, "+", "", 2},
+		{RIGHT_PAREN, ")", "", 2},
+	}
+
+	if len(expected) != len(s.Tokens) {
+		t.Logf("failed scanning tokens: number of tokens are wrongt: expected=%d received=%d\n", len(expected), len(s.Tokens))
+		failed = true
+	}
+
+	for i, token := range s.Tokens {
+		if token != expected[i] {
+			t.Logf("failed scanning tokens: expected=%v received=%v\n", token, expected[i])
+			failed = true
+		}
+	}
+
+	if failed {
+		t.FailNow()
+	}
+}
