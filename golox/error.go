409c088bcf17af88b5cc8309f720ac6d125083d8
David Kuda <davidkuda3@gmail.com>
2026-04-22 23:17:49 +0200

    experiment with further error ideas

--- golox/error.go
+++ golox/error.go
@@ -2,22 +2,42 @@ package golox
 
 import "fmt"
 
-// TODO: Error might be confused with go error.
-// either there is a better name, or use go errors?
-type Error struct {
+type GoloxError struct {
 	line    int
 	message string
 	where   string
 }
 
-func NewError(line int, message string) *Error {
-	return &Error{
+func NewError(line int, message string) *GoloxError {
+	return &GoloxError{
 		line:    line,
 		message: message,
 		where:   "",
 	}
 }
 
-func (e *Error) Report() {
+func (e *GoloxError) Error() string {
+	return fmt.Sprintf("[line %d] Error %s: %s\n", e.line, e.where, e.message)
+}
+
+func ReportGoloxError(token Token, message string) {
+	if token.Type == EOF {
+		fmt.Printf(
+			"[line %d] Error %s: %s\n",
+			token.Line,
+			"at end",
+			message,
+		)
+	} else {
+		fmt.Printf(
+			"[line %d] Error at %s: %s\n",
+			token.Line,
+			token.Lexeme,
+			message,
+		)
+	}
+}
+
+func (e *GoloxError) Report() {
 	fmt.Printf("[line %d] Error %s: %s\n", e.line, e.where, e.message)
 }
