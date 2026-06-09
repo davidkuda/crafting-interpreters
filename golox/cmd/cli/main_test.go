9b0491dc7a082a0b97e02165fbb26567fc22769f
David Kuda <davidkuda3@gmail.com>
2026-06-09 22:07:21 +0200

    test cli: use lox scripts to test the CLI

---
+++ golox/cmd/cli/main_test.go
@@ -0,0 +1,58 @@
+package main_test
+
+import (
+	"os/exec"
+	"strings"
+	"testing"
+)
+
+func TestCLI(t *testing.T) {
+	var tests = []struct {
+		file     string
+		expected string
+	}{
+		{
+			file:     "./testscripts/8_vars.lox",
+			expected: "42",
+		},
+		{
+			file:     "./testscripts/9_controlflow_ifelse.lox",
+			expected: "yes, true, indeed.",
+		},
+		{
+			file:     "./testscripts/blocks.lox",
+			expected: "inner a\nouter b\nglobal c\nouter a\nouter b\nglobal c\nglobal a\nglobal b\nglobal c",
+		},
+	}
+
+	var failed bool
+
+	for _, test := range tests {
+
+		cmd := exec.Command(
+			"go",
+			"run",
+			".",
+			test.file,
+		)
+
+		output, err := cmd.CombinedOutput()
+		if err != nil {
+			t.Logf("executing script %q failed: %v", test.file, err)
+			failed = true
+			continue
+		}
+
+		got := strings.TrimSpace(string(output))
+		want := test.expected
+
+		if got != want {
+			t.Logf("failed %s: got %q, want %q", test.file, got, want)
+			failed = true
+		}
+	}
+
+	if failed {
+		t.Fatal("failed TestCLI")
+	}
+}
